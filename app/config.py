eefa24cd2d82b581c91b03342b0f55a11ac128bf
Maksym Tomashivskyi <max.tom2777@gmail.com>
2025-11-26 16:59:15 +0200

    Clear sensitive fields in config.py
    Removed sensitive information from configuration.

--- app/config.py
+++ app/config.py
@@ -3,14 +3,14 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # Секретний ключ для генерації токенів
-    SECRET_KEY: str = "secret_key_for_lab_4_change_me"
+    SECRET_KEY: str = 
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 
 
     # Налаштування пошти (для імітації або реальної відправки)
-    MAIL_USERNAME: str = "supforgraduatebook@gmail.com"
-    MAIL_PASSWORD: str = "xnwy tqkc kmar lgfd"
-    MAIL_FROM: str = "Gradebook Admin"
+    MAIL_USERNAME: str = 
+    MAIL_PASSWORD: str = 
+    MAIL_FROM: str = 
     MAIL_PORT: int = 587
     MAIL_SERVER: str = "smtp.gmail.com"
 
